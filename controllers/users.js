536384cb613e02127ddf36846ccc8174dfcfb642
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-09-18 12:38:48 -0700

    create regEx and CreateUser - tests ok in Postman and Compass

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,55 @@
 // controllers/users.js
 /* --------------------------------- imports -------------------------------- */
-const path = require('path');
-const getDataFromFile = require('../helpers/files');
+const path = require("path");
+const getDataFromFile = require("../helpers/files");
+const User = require("../models/user");
 
 /* ---------------------------------- path ---------------------------------- */
-const dataPath = path.join(__dirname, '..', 'data', 'users.json');
+const dataPath = path.join(__dirname, "..", "data", "users.json");
 
 /* -------------------------------- functions ------------------------------- */
-const getUsers = (req, res) => getDataFromFile(dataPath)
-  .then((users) => res.status(200).send(users))
-  .catch(() => res.status(500).send({ message: 'An error has occurred on the server' }));
+const getUsers = (req, res) =>
+  getDataFromFile(dataPath)
+    .then((users) => res.status(200).send(users))
+    .catch(() =>
+      res.status(500).send({ message: "An error has occurred on the server" })
+    );
 
-const sendUserProfile = (req, res) => getDataFromFile(dataPath)
-  .then((users) => users.find((user) => user._id === req.params.id))
+const sendUserProfile = (req, res) =>
+  getDataFromFile(dataPath)
+    .then((users) => users.find((user) => user._id === req.params.id))
 
-  .then((user) => {
-    if (!user) {
-      res.status(404).send({ message: 'User ID not found' });
-    }
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: "User ID not found" });
+      }
 
-    res.status(200).send(user);
-  })
-  .catch(() => res.status(500).send({ message: 'An error has occurred on the server' }));
+      res.status(200).send(user);
+    })
+    .catch(() =>
+      res.status(500).send({ message: "An error has occurred on the server" })
+    );
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+
+    .catch((err) => {
+      if (err.name ==='ValidationError'){
+        res.status(400).send({
+          message: `${Object.values(err.errors)
+            .map((error)=>error.message)
+            .join(', ')}`,
+        });
+      } else {
+      res.status(500).send({ message: "An error has occurred on the server" });
+      }
+});
+};
 
 /* --------------------------------- exports -------------------------------- */
-module.exports = { getUsers, sendUserProfile };
+module.exports = { getUsers, sendUserProfile, createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 // routes/users.js
 /* --------------------------------- imports -------------------------------- */
 const router = require('express').Router();
-const { getUsers, sendUserProfile } = require('../controllers/users');
+const { getUsers, sendUserProfile, createUser } = require('../controllers/users');
 
 /* --------------------------------- routes --------------------------------- */
 router.get('/users', getUsers);
 
 router.get('/users/:id', sendUserProfile);
+
+router.post('/users', createUser)
 
 /* --------------------------------- exports -------------------------------- */
 module.exports = router;
